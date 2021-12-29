a54278fbf575771ca650bc7db28cc192ae22ec53
yoursaint <yoursaint@cnu.ac.kr>
2021-12-30 00:07:29 +0900

    add module tsc-watch

--- dist/index.js
+++ dist/index.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const name = "yoursaint", age = 25, gender = "male";
 const sayHi = (name, age, gender) => {
-    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+    return `Hello ${name}, you are ${age}, you are a ${gender}`;
 };
-sayHi(name, age, gender);
+console.log(sayHi(name, age, gender));
 //# sourceMappingURL=index.js.map
