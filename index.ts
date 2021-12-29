78f37fdb3ab96a6c2cbef5414714454ec7212c15
yoursaint <yoursaint@cnu.ac.kr>
2021-12-29 23:57:49 +0900

    type in typescript

--- index.ts
+++ index.ts
@@ -2,10 +2,10 @@ const name = "yoursaint",
     age = 25,
     gender = "male";
 
-const sayHi = (name, age, gender?) => {
-    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`)
+const sayHi = (name:string, age:number, gender:string): string => {
+    return `Hello ${name}, you are ${age}, you are a ${gender}`
 }
 
-sayHi(name, age, gender);
+console.log(sayHi(name, age, gender));
 
 export {};
